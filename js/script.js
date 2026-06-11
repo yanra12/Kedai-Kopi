16a6356c093bc48bbadadbed1ff4a4b2d0b03ac7
Yanra Dea Maulana <yanradea04@gmail.com>
2026-06-11 20:54:22 +0700

    website kedai kopi

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
+  navbarNav.classList.toggle("active");
 };
 
 // klik diluar sidebar untuk menghilangkan nav
 
 const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener("click", function(e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove("active");
-    }
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
